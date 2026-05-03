d579975d25d2ce9c4e6436bd11400865c3dd3781
Xerkara <ibrahimserkara@gmail.com>
2026-05-03 18:49:52 +0300

    github functions-methods assignment completed

---
+++ 2-js-basics/2-functions-methods/functions-practice.js
@@ -0,0 +1,40 @@
+function sayHello() {
+    console.log("Hello!");
+}
+
+function introduceYourself(name) {
+    console.log("Hi, my name is " + name + ".");
+}
+
+function greetPerson(name, greeting = "Hello") {
+    console.log(`${greeting}, ${name}!`);
+}
+
+function addNumbers(num1, num2) {
+    return num1 + num2;
+}
+
+function createFullName(firstName, lastName) {
+    const fullName = `${firstName} ${lastName}`;
+    return fullName;
+}
+
+function calculateTip(billAmount, tipPercentage = 15) {
+    const tipAmount = billAmount * (tipPercentage / 100);
+    return tipAmount;
+}
+
+// Test your functions here
+sayHello();
+introduceYourself("Sarah");
+greetPerson("Alex");
+greetPerson("Maria", "Hi");
+
+const sum = addNumbers(5, 3);
+console.log(`The sum is: ${sum}`);
+
+const fullName = createFullName("John", "Doe");
+console.log(`Full name: ${fullName}`);
+
+const tip = calculateTip(50);
+console.log(`Tip for $50 bill: $${tip}`);
